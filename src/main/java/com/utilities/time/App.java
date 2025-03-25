9fe42c9d53cbaaf292582b99be41329bf5b1ef04
Anderson Acuña <andersonyhavs@outlook.com>
2025-03-25 12:05:53 -0500

    Add basic timer for task execution.

--- src/main/java/com/utilities/time/App.java
+++ src/main/java/com/utilities/time/App.java
@@ -1,9 +1,23 @@
 package com.utilities.time;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class App {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        timer(1);
+    }
+
+    public static void timer(int minutes) {
+        Timer temporizer = new Timer();
+
+        temporizer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("The time is up.");
+            }
+        }, minutes * 60 * 1000, minutes * 60 * 1000);
     }
 }
